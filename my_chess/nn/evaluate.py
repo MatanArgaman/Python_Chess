5ee69fcc5bc17394a22316ed5d95bac2141ede56
Matan Argaman <matanargaman@gmail.com>
2021-02-01 22:41:02 +0200

    1. Added logging of evaluation values during training. 2. updated evaluation function.

--- my_chess/nn/evaluate.py
+++ my_chess/nn/evaluate.py
@@ -6,56 +6,69 @@ import chess
 import json
 from tensorflow import keras
 import tensorflow as tf
-from play import get_nn_moves
+from play import get_nn_moves_and_probabilities
 from shared.shared_functionality import get_fen_moves_and_probabilities
 
 
-def evaluate_nn():
+def evaluate_nn(total_samples = None):
     # 1_6 is test
     with open(os.path.join(os.getcwd(), '../', 'config.json'), 'r') as f:
         config = json.load(f)
 
     nn_model = keras.models.load_model(config['train']['nn_model_path'])
 
-    c = 0
+    counter = [0]
     score_list = []
     for i in range(10):
         for j in range(10):
-            print('starting file', i, j, time.time())
-            con_data = config['database']
-            with open(os.path.join(con_data['file_path'],
-                                   con_data['file_name'] + '{0}_{1}.pkl'.format(i, j)), 'rb') as f:
-                d = pickle.load(f)
+            score = single_file_evaluate(nn_model, score_list, config, total_samples, counter, i,i, k_best_moves=5)
+            print("score:", score)
+            counter = [0]
+            score_list = []
+            # print('starting file', i, j, time.time())
+            # expert_set=set(expert_moves)
+            # nn_set=set(nn_moves)
+            # denominator=expert_set.union(nn_set)
+            # nominator=expert_set.intersection(nn_set)
+            # score = len(nominator)/len(denominator)
+            # score_list.append(score)
 
+
+def single_file_evaluate(nn_model, score_list, config, total_samples, counter, file_index1, file_index2, k_best_moves=1):
+    con_data = config['database']
+    with open(os.path.join(con_data['file_path'],
+                           con_data['file_name'] + '{0}_{1}.pkl'.format(file_index1, file_index2)), 'rb') as f:
+        d = pickle.load(f)
+
+    expert_moves_list = []
+    board_fen_list = []
+    for l, (board_fen, v) in enumerate(d.items()):
+        expert_moves, probabilities = get_fen_moves_and_probabilities(d, board_fen)
+        expert_moves= [m[:-1] if 'q' in m else m for m in expert_moves]
+        expert_moves_list.append(expert_moves)
+        board_fen_list.append(board_fen)
+
+        counter[0] += 1
+        if counter[0] % 10000 == 0 or l == len(d.keys()) - 1:
+            nn_moves, _ = get_nn_moves_and_probabilities([chess.Board(bf) for bf in board_fen_list], nn_model,
+                                                         k_best_moves=k_best_moves)
+            for k in range(len(nn_moves)):
+                try:
+                    if set(nn_moves[k]).intersection(expert_moves_list[k]):
+                        score_list.append(1)
+                    else:
+                        score_list.append(0)
+                except:
+                    pass
+
+            # score = np.array(score_list)
+            # print('score:', score.mean())
             expert_moves_list = []
             board_fen_list = []
-            for l, (board_fen, v) in enumerate(d.items()):
-                expert_moves, probabilities = get_fen_moves_and_probabilities(d, board_fen)
-                expert_moves_list.append(expert_moves)
-                board_fen_list.append(board_fen)
-
-                c += 1
-                if c % 10000 == 0 or l == len(d.keys()) - 1:
-                    nn_moves = get_nn_moves([chess.Board(bf) for bf in board_fen_list], nn_model, k_best_moves=1)
-                    for k in range(len(nn_moves)):
-                        if nn_moves[k] and nn_moves[k][0] in expert_moves_list[k]:
-                            score_list.append(1)
-                        else:
-                            score_list.append(0)
-
-                    score = np.array(score_list)
-                    print('score:', score.mean())
-                    expert_moves_list = []
-                    board_fen_list = []
-
-                # expert_set=set(expert_moves)
-                # nn_set=set(nn_moves)
-                # denominator=expert_set.union(nn_set)
-                # nominator=expert_set.intersection(nn_set)
-                # score = len(nominator)/len(denominator)
-                # score_list.append(score)
+        if total_samples is not None and counter[0] > total_samples:
+            return np.array(score_list).mean()
 
 
 if __name__ == '__main__':
     print('gpu available:', tf.test.is_gpu_available())
-    evaluate_nn()
+    evaluate_nn(total_samples=10002)
--- my_chess/nn/train_network.py
+++ my_chess/nn/train_network.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
+from tensorflow.keras.layers import LeakyReLU
 import matplotlib.pyplot as plt
 import os
 import json
+import argparse
 
 from shared.shared_functionality import INPUT_PLANES, OUTPUT_PLANES
-from scipy.sparse import load_npz, csr_matrix
+from scipy.sparse import load_npz
 from predict import get_output_representation
+from nn.evaluate import single_file_evaluate
 
 
 def get_model():
@@ -30,6 +33,9 @@ def get_nn_io_file(index1, index2, is_input=True):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--load', action='store_true', help='continue training from existing network')
+    args = parser.parse_args()
 
     index1_order = np.random.permutation(10)
     index2_order = np.random.permutation(10)
@@ -37,33 +43,47 @@ if __name__ == '__main__':
     with open(os.path.join(os.getcwd(), 'config.json'), 'r') as f:
         config = json.load(f)
 
+    train_writer = tf.summary.create_file_writer(os.path.join(config['train']['nn_model_path'], 'train'))
+    test_writer = tf.summary.create_file_writer(os.path.join(config['train']['nn_model_path'], 'test'))
+
+
     con_train = config['train']
 
-    model = get_model()
+    if args.load:
+        model = keras.models.load_model(con_train['nn_model_path'])
+    else:
+        model = get_model()
 
-    for epoch in range(5):
+    index1_test = con_train['test_index1']
+    index2_test = con_train['test_index2']
+    step = 0
+    for epoch in range(20):
         counter = 0
         for a, i in enumerate(index1_order):
             for b, j in enumerate(index2_order):
-                if i == con_train['test_index1'] and j == con_train['test_index2']:
+                if i == index1_test and j == index2_test:
                     continue  # leave the last file for test
+
+                train_score = single_file_evaluate(model, [], config,10002, [0], i, j)
+                test_score = single_file_evaluate(model, [], config,10002, [0], index1_test, index2_test)
+                print("train:", round(train_score,3))
+                print("test:", round(test_score,3))
+                train_writer.flush()
+                test_writer.flush()
+
+                with train_writer.as_default():
+                    tf.summary.scalar("score", train_score, step=step)
+                with test_writer.as_default():
+                    tf.summary.scalar("score", test_score, step=step)
+
                 x_train = get_nn_io_file(i, j, is_input=True)
                 y_train = get_nn_io_file(i, j, is_input=False)
                 x_train = x_train.toarray().reshape([8, 8, -1, INPUT_PLANES]).swapaxes(0, 2).swapaxes(1, 2)
                 y_train = y_train.toarray().reshape([8, 8, -1, OUTPUT_PLANES]).swapaxes(0, 2).swapaxes(1, 2)
+                step += x_train.shape[0]
                 model.fit(x_train, y_train, epochs=1)
                 model.save(con_train['nn_model_path'])
                 del model
                 model = keras.models.load_model(con_train['nn_model_path'])
-
                 print("epoch:", epoch, "round:", counter, '/', '99')
                 counter += 1
-
-    index1_test = con_train['test_index1']
-    index2_test = con_train['test_index2']
-
-    x_test = get_nn_io_file(index1_test, index2_test, is_input=True)
-    y_test = get_nn_io_file(index1_test, index2_test, is_input=False)
-    x_test = x_test.toarray().reshape([8, 8, -1, INPUT_PLANES]).swapaxes(0, 2).swapaxes(1, 2)
-    y_test = y_test.toarray().reshape([8, 8, -1, OUTPUT_PLANES]).swapaxes(0, 2).swapaxes(1, 2)
-    model.train(x_test, y_test)
