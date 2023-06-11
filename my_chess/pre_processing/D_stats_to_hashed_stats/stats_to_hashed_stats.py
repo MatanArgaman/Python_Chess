64e81f355bbc8ec3a43c62ce5948eb364882651b
Matan Argaman <matan@shopic.co>
2023-06-11 09:33:47 +0300

    small

--- my_chess/pre_processing/D_stats_to_hashed_stats/stats_to_hashed_stats.py
+++ my_chess/pre_processing/D_stats_to_hashed_stats/stats_to_hashed_stats.py
@@ -6,11 +6,11 @@ import os
 import re
 
 
-def main(path):
+def main(in_path, out_path):
     IN_FILENAME = "stat{0}_{1}.pkl"
     OUT_FILENAME = "cstat{0}.pkl"
 
-    stat_files =[f for f in Path(path).rglob("stat*.pkl")]
+    stat_files =[f for f in Path(in_path).rglob("stat*.pkl")]
     same_index2_files = {}
     for f in stat_files:
         filename = os.path.basename(f)
@@ -20,11 +20,11 @@ def main(path):
         same_index2_files[index2] = same_index2_files.get(index2, [])
         same_index2_files[index2].append(filename)
     for index2, filename_list in same_index2_files.items():
-        full_path = os.path.join(path, filename_list[0])
+        full_path = os.path.join(in_path, filename_list[0])
         with open(full_path, 'rb') as fp:
             stat_dict = pickle.load(fp)
         for filename in filename_list[1:]:
-            full_path = os.path.join(path, filename)
+            full_path = os.path.join(in_path, filename)
             with open(full_path, 'rb') as fp:
                 additional_stat_dict = pickle.load(fp)
                 for board_fen, move_dict in additional_stat_dict.items():
@@ -40,13 +40,14 @@ def main(path):
                                 stat_fen[move]['wins']+=win_loss_draw['wins']
                                 stat_fen[move]['draws']+=win_loss_draw['draws']
                                 stat_fen[move]['losses']+=win_loss_draw['losses']
-        out_path = os.path.join(path, OUT_FILENAME.format(index2))
+        out_path = os.path.join(out_path, OUT_FILENAME.format(index2))
         with open(out_path, "wb") as fp:
             pickle.dump(stat_dict, fp)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('path')
+    parser.add_argument('in_path')
+    parser.add_argument('out_path')
     args = parser.parse_args()
-    main(args.path)
+    main(args.in_path,args.out_path)
